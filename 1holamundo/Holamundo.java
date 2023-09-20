a9a4571bc6e3ee7e6e1d48286f2eac315dba39d0
Dari Kawer <satanstarko@hotmail.com>
2023-09-19 20:56:52 -0600

    primer holamundo actualizado papu

---
+++ 1holamundo/Holamundo.java
@@ -0,0 +1,6 @@
+public class Holamundo{
+    public static void main(String[] args){
+        System.out.println("Hola Mundo, ya aprendi mi primer hola mundo en java v:");
+        System.out.println("Ya soy senior developer papu");
+    }
+}
