74e2d44c63ad42e528f1128aa02bbb5025b3fc2f
Dari Kawer <satanstarko@hotmail.com>
2023-11-15 00:27:20 -0600

    AHORA SI TERMINADISIMO .

--- pinpong/MainPong.java
+++ pinpong/MainPong.java
@@ -1,16 +1,19 @@
 public class MainPong {
     /*
+     * Del profe:
      * Ping Pong
      * -2 jugadores
      * -Scroll
      * -2-3 partidas
      * -Score
      * 
-     * lo mio
+     * lo mio:
      * -3 dificultades
      * -facil - la pelota ira lenta
      * -medio - la pelota ira rapida
      * -dficil - la pelota ira super rapido
+     * -menu con las dificultades
+     * -boton para salir en el menu
      */
     
     public static void main(String[] args) {
